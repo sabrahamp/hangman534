b0b15e404b315b50041a86219161367b47703a90
Shibu Abraham <sabrahamp@hotmail.com>
2023-11-22 12:14:42 +0000

    After completing the Task-3 now committing & pushing changes to git-hub

--- milestone_2.py
+++ milestone_2.py
@@ -1,25 +1,33 @@
 import random
-
+word = ''
 my_favourite_fruits = ['Mango', 'Jackfruit', 'Strawberry', 'Grapes', 'Pineapple']
-
 word_list = my_favourite_fruits
-
 print(my_favourite_fruits)
-
 print(my_favourite_fruits[3])
-
-word = random.choice(word_list)
-
+word = random.choice(word_list).lower()
 print(word)
 
-guess ='%£'
-print(len(guess))
-while(len(guess) != 1 or not guess.isalpha()):
-    guess = input("Enter your value as one Alpabetical char: ")
-    print(guess)
-    if len(guess) == 1 and guess.isalpha():
-        print('Good guess!')
-    else:
-        print('Ooops!, That is not a valid input.')
-        continue
-        
+def check_guess(guess):
+    guess = guess.lower()
+    for char in word:
+        if guess == char:
+            print(guess, ' was found in ', word)
+            break
+
+def ask_for_input():
+    guess = '  '
+    print(len(guess))
+    while(len(guess) != 1 or not guess.isalpha()):
+        guess = input("Enter your value as one Alpabetical char: ")
+        print(guess)
+        if len(guess) == 1 and guess.isalpha():
+            print('Good guess!')
+        else:
+            print('Ooops!, That is not a valid input.')
+            continue
+    check_guess(guess)
+
+
+
+
+ask_for_input()
